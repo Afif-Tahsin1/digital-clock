898a8bed269089bba9c03a1ca6a31e7da319fc89
hhh <jareen0512@gmail.com>
2025-11-21 16:33:07 +0600

    digital clock

---
+++ script.js
@@ -0,0 +1,20 @@
+function updateClock(){
+
+    const date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    let am_pm = "AM";
+
+    if (hours >= 12){
+        am_pm = "PM";
+        if (hours > 12){
+            hours = hours - 12;
+        }
+    }
+    const clockElement = document.getElementById('digital-clock');
+    clockElement.textContent = `${hours}:${minutes}:${seconds} ${am_pm}`;
+}
+setInterval(updateClock, 1000);
+updateClock();
